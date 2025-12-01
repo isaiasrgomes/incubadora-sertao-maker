2c8c8c1ae65245913c555ec16f17dfc78ffb3585
Isaias Gomes <isaias.robson0@gmail.com>
2025-12-01 11:14:28 -0300

    change BrowserRouter to HashRouter at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={import.meta.env.BASE_URL} element={<Index />} />
           <Route path={`${import.meta.env.BASE_URL}programa/:id`} element={<ProgramaDetails />} />
@@ -32,7 +32,7 @@ const App = () => (
           {/* rotas personalizadas devem ficar acima desta */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
